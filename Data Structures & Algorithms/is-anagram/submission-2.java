fd3c39d4c8bfd0d4bb426219f04a27b3050417e4
hrohdU <dmytro.hrohozhnyk@city.ac.uk>
2026-09-03 15:18:45 +0100

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] ss = s.toCharArray();
+        char[] tt = t.toCharArray();
+        Arrays.sort(ss);
+        Arrays.sort(tt);
+        
+        if (Arrays.equals(ss,tt)){
+            return true;
+        }
+        else {return false;}
+    }
+}
